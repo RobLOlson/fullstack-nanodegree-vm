3a1eeaa0f3136e106153853c8648d8d6dd7e255b
Robert <rob.louis@gmail.com>
2019-06-12 13:28:00 -0800

    added full gspread functionality

--- finalproject.py
+++ finalproject.py
@@ -4,7 +4,10 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 
 app = Flask(__name__)
 
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+scope = ["https://spreadsheets.google.com/feeds",
+         'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 
@@ -17,9 +20,6 @@ sheet_time = spreadsheet.worksheet("time_stamp")
 
 local_db = {}
 
-# update_r = sheet_time.cell(2, 1)
-# update_m = sheet_time.cell(2, 2)
-
 
 class GRestaurant(object):
     def __init__(self, name, id):
@@ -29,9 +29,10 @@ class GRestaurant(object):
     @property
     def serialize(self):
         return {
-            "name" : self.name,
-            "id" : self.id
+            "name": self.name,
+            "id": self.id
         }
+
 
 class GMenu_Item(object):
     def __init__(self, name, id, price=None, description=None, restaurant_id=None):
@@ -44,24 +45,27 @@ class GMenu_Item(object):
     @property
     def serialize(self):
         return {
-            "name" : self.name,
-            "id" : self.id,
-            "price" : self.price,
-            "description" : self.description,
-            "restaurant_id" : self.restaurant_id
+            "name": self.name,
+            "id": self.id,
+            "price": self.price,
+            "description": self.description,
+            "restaurant_id": self.restaurant_id
         }
+
 
 @app.route("/")
 @app.route("/restaurants/")
 def all_restaurants():
     global local_db
-    return render_template("allrestaurants.html", local_db = local_db)
+    return render_template("allrestaurants.html", local_db=local_db)
+
 
 @app.route("/restaurant/<restaurant_id>/")
 @app.route("/restaurant/<restaurant_id>/menu/")
 def restaurant_menu(restaurant_id):
     global local_db
 
+    restaurant = local_db[restaurant_id]
     items = []
 
     for key in local_db.keys():
@@ -69,7 +73,8 @@ def restaurant_menu(restaurant_id):
             if local_db[key].restaurant_id == restaurant_id:
                 items.append(local_db[key])
 
-    return render_template("menu.htm", restaurant=local_db[restaurant_id], items=items)
+    return render_template("menu.htm", restaurant=restaurant, items=items)
+
 
 @app.route("/restaurant/<restaurant_id>/edit/", methods=["GET", "POST"])
 def edit_restaurant(restaurant_id):
@@ -84,17 +89,17 @@ def edit_restaurant(restaurant_id):
         time_stamp = time.time()
         target_row = sheet_r.find(restaurant.id).row
         sheet_r.update_cell(target_row, 1, value=request.form['name'])
-        sheet_time.update_cell(1, 1, time_stamp)
-
-        local_db[restaurant.id].name = request.form['name']
+        sheet_time.update_cell(1, 1, round(time_stamp))
 
         with shelve.open("database") as db:
-            db[restaurant.id].name = request.form['name']
+            db[restaurant.id] = GRestaurant(request.form['name'], restaurant_id)
+            local_db = dict(db)
 
         return redirect(url_for("all_restaurants"))
 
     else:
         return render_template("editrestaurant.html", restaurant=restaurant)
+
 
 @app.route("/restaurant/<restaurant_id>/delete/", methods=["GET", "POST"])
 def delete_restaurant(restaurant_id):
@@ -105,7 +110,17 @@ def delete_restaurant(restaurant_id):
     restaurant = local_db[restaurant_id]
 
     if request.method == "POST":
-        return "POSTED"
+        time_stamp = time.time()
+        target_row = sheet_r.find(restaurant.id).row
+        sheet_r.delete_row(target_row)
+        sheet_time.update_cell(1, 1, round(time_stamp))
+
+        with shelve.open("database") as db:
+            del db[restaurant.id]
+            local_db = dict(db)
+
+        return redirect(url_for("all_restaurants"))
+
     else:
         return render_template("deleterestaurant.html", restaurant=restaurant)
 
@@ -126,22 +141,134 @@ def new_restaurant():
 
         sheet_r.append_row([request.form["name"], new_id])
         sheet_time.update_cell(1, 1, time_stamp)
-        local_db[new_id] = GRestaurant(request.form["name"], new_id)
+
+        with shelve.open("database") as db:
+            db[new_id] = GRestaurant(request.form["name"], new_id)
+            local_db = dict(db)
         return redirect(url_for("all_restaurants"))
+
     else:
         return render_template("newrestaurant.html")
 
-@app.route("/restaurant/<restaurant_id>/menu/new/")
+@app.route("/restaurant/<restaurant_id>/menu/new/", methods=["GET", "POST"])
 def new_menu_item(restaurant_id):
-    return "new menu item"
+    global local_db
+    global sheet_r
+    global sheet_m
+    global sheet_time
 
-@app.route("/restaurant/<restaurant_id>/menu/<menu_id>/edit/")
+    restaurant = local_db[restaurant_id]
+
+    if request.method == "POST":
+        new_id = 1
+        time_stamp = time.time()
+
+        while "menuid"+str(new_id) in local_db.keys():
+            new_id = random.randint(1, 1000000)
+
+        new_id = "menuid"+str(new_id)
+
+        new_menu_item = GMenu_Item(request.form["name"],
+                                   new_id,
+                                   request.form["price"],
+                                   request.form["description"],
+                                   restaurant_id)
+        sheet_m.append_row([new_menu_item.name,
+                            new_menu_item.id,
+                            new_menu_item.price,
+                            new_menu_item.description,
+                            new_menu_item.restaurant_id])
+
+        sheet_time.update_cell(1, 1, time_stamp)
+
+        with shelve.open("database") as db:
+            db[new_id] = new_menu_item
+            local_db = dict(db)
+
+        return redirect(url_for("restaurant_menu", restaurant_id=restaurant.id))
+
+    else:
+        return render_template("newmenuitem.html", restaurant=restaurant)
+
+@app.route("/restaurant/<restaurant_id>/menu/<menu_id>/edit/", methods=["GET", "POST"])
 def edit_menu_item(restaurant_id, menu_id):
-    return "edit menu item"
+    global local_db
+    global sheet_r
+    global sheet_m
+    global sheet_time
 
-@app.route("/restaurant/<restaurant_id>/menu/<menu_id>/delete/")
+    restaurant = local_db[restaurant_id]
+    menu_item = local_db[menu_id]
+
+    if request.method == "POST":
+        time_stamp = round(time.time())
+        target_row = sheet_m.find(menu_id).row
+        sheet_m.delete_row(target_row)
+        sheet_m.append_row([request.form["name"],
+                            menu_id,
+                            request.form["price"],
+                            request.form["description"],
+                            restaurant_id])
+        sheet_time.update_cell(1, 1, time_stamp)
+
+        with shelve.open("database") as db:
+            db[menu_id] = GMenu_Item(request.form["name"],
+                                     menu_id,
+                                     request.form["price"],
+                                     request.form["description"],
+                                     restaurant_id)
+            local_db = dict(db)
+
+        return redirect(url_for("restaurant_menu", restaurant_id=restaurant_id))
+
+    else:
+        return render_template("editmenuitem.html", restaurant=restaurant, menu_item=menu_item)
+
+
+@app.route("/restaurant/<restaurant_id>/menu/<menu_id>/delete/", methods=["GET", "POST"])
 def delete_menu_item(restaurant_id, menu_id):
-    return "delete menu item"
+    global local_db
+    global sheet_r
+    global sheet_m
+    global sheet_time
+
+    restaurant = local_db[restaurant_id]
+    menu_item = local_db[menu_id]
+
+    if request.method == "POST":
+        time_stamp = round(time.time())
+        target_row = sheet_m.find(menu_id).row
+        sheet_m.delete_row(target_row)
+        sheet_time.update_cell(1, 1, time_stamp)
+
+        with shelve.open("database") as db:
+            del db[menu_id]
+            local_db = dict(db)
+
+        return redirect(url_for("restaurant_menu", restaurant_id=restaurant_id))
+    else:
+        return render_template("deletemenuitem.html", restaurant=restaurant, menu_item=menu_item)
+
+@app.route("/restaurant/<restaurant_id>/JSON/")
+def restaurant_menu_JSON(restaurant_id):
+    global local_db
+
+    restaurant = local_db[restaurant_id]
+    items = []
+
+    for key in local_db.keys():
+        if "menuid" in key:
+            if local_db[key].restaurant_id == restaurant_id:
+                items.append(local_db[key])
+
+    return jsonify(menu_items=[i.serialize for i in items])
+
+
+@app.route("/restaurant/<restaurant_id>/menu/<menu_id>/JSON/")
+def menu_item_JSON(restaurant_id, menu_id):
+    global local_db
+
+    return jsonify(menu_item=local_db[menu_id].serialize)
 
 def full_update_local():
     global sheet_r
@@ -164,15 +291,21 @@ def full_update_local():
                                        description=row["description"],
                                        restaurant_id=row["restaurant_id"])
 
-        db["last_updated_time_stamp"] = int(sheet_time.cell(1,1).value)
+        db["last_updated_time_stamp"] = int(sheet_time.cell(1, 1).value)
         last_updated_time_stamp = db["last_updated_time_stamp"]
         local_db = dict(db)
+
+# end of full_update_local() ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 
 with shelve.open("database") as db:
     last_updated_time_stamp = db.get("last_updated_time_stamp", 0)
     local_db = dict(db)
 
-if int(last_updated_time_stamp) < int(sheet_time.cell(1, 1).value):
+if int(last_updated_time_stamp) < int(sheet_time.cell(1, 1).value) \
+   or\
+   int(sheet_time.cell(1, 1).value) == 0:
+
    full_update_local()
 
 
